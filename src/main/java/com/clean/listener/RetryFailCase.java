8d2239267e810d48b2d51d5b982032a97daba97c
X1-kkk <xuankhoi999@gmailc.com>
2023-12-25 10:11:05 +0700

    create new retryfailedcase class

---
+++ src/main/java/com/clean/listener/RetryFailCase.java
@@ -0,0 +1,20 @@
+package com.clean.listener;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryFailCase implements IRetryAnalyzer {
+
+    private int counter = 0;
+    private int retryLimit = 4;
+
+    @Override
+    public boolean retry(ITestResult iTestResult) {
+        if(counter < retryLimit)
+        {
+            counter++;
+            return true;
+        }
+        return false;
+    }
+}
